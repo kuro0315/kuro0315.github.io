725b94ab705a35c12cab0017eafecb3656f7ff71
kuro0315 <skuro0315@gmail.com>
2024-06-08 11:12:13 +0900

    chore: Translate README.md and notes.tsx to Japanese

--- app/data/notes.tsx
+++ app/data/notes.tsx
@@ -11,7 +11,15 @@ export interface Note {
 		title: 'OpenGLでglGetErrorエラー', 
 		content: 'おそらくこれに近い https://github.com/MPI-IS/mesh/issues/23' 
 	},
-	{ id: 2, title: 'メモ 2', content: 'これはメモ 2 の内容です。' },
-	{ id: 3, title: 'メモ 3', content: 'これはメモ 3 の内容です。' },
+	{ 
+		id: 2, 
+		title: 'メモ 2', 
+		content: 'これはメモ 2 の内容です。' 
+	},
+	{ 
+		id: 3, 
+		title: 'メモ 3', 
+		content: 'これはメモ 3 の内容です。' 
+	},
   ];
   
